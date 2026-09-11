b2724fae6eda0bf792ab605622e036cd2522cfce
Matroskin <matroskin.usatyj2009@gmail.com>
2026-09-11 20:34:23 +0200

    mala poprawka

--- algorithms/simplex.py
+++ algorithms/simplex.py
@@ -1,10 +1,10 @@
 # Symulacja Algorytmu Simplex (Optymalizacja liniowa)
-print("--- Algorytm Simplex ---")
-print("Cel: Maksymalizacja zysku Z = 3*x + 5*y")
-print("Ograniczenia:")
-print("1) x <= 4")
-print("2) 2*y <= 12")
-print("3) 3*x + 2*y <= 18")
+print("""--- Algorytm Simplex ---
+Cel: Maksymalizacja zysku Z = 3*x + 5*y
+Ograniczenia:
+1) x <= 4
+2) 2*y <= 12
+3) 3*x + 2*y <= 18""")
 
 x_opt = 2
 y_opt = 6
